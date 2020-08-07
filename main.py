4907dd477d9d4be45158155dd7bb9412cd5264b2
Andrea Odetti <mariofutire@gmail.com>
2020-08-07 13:54:45 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+import sqlite3
+import io
+
+import pandas as pd
+
+
+def get_result():
+    command = ['speedtest', '-f', 'csv', '--output-header']
+    output = subprocess.run(command, capture_output=True)
+    stdout = output.stdout.decode()
+    return stdout
+
+
+def dummy():
+    a = '"server name","server id","latency","jitter","packet loss","download","upload","download bytes","upload bytes","share url"\n"ISP - Server","12345","7.273","3.599","N/A","13971184","1244924","130792296","4863832","https://www.speedtest.net"\n'
+    return a
+
+
+def main():
+    data = get_result()
+    # data = dummy()
+    f = io.StringIO(data)
+    df = pd.read_csv(f)
+    df['date'] = pd.Timestamp.now()
+
+    this_folder = os.path.dirname(__file__)
+    path = os.path.join(this_folder, 'data.db')
+
+    with sqlite3.connect(path) as db:
+        df.to_sql('data', db, if_exists='append')
+
+
+if __name__ == '__main__':
+    main()
---
+++ systemd/report.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+python /home/pi/projects/speedreport/main.py
